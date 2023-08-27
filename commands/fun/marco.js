af97e7f096e5def354990598579bb72a62bdce15
HRTucker <25648829-HRTucker@users.noreply.replit.com>
2023-08-27 16:05:18 +0000

    Initial commit

---
+++ commands/fun/marco.js
@@ -0,0 +1,11 @@
+const {SlashCommandBuilder} = require('discord.js');
+
+module.exports = {
+  cooldown: 5,
+  data: new SlashCommandBuilder()
+    .setName('marco')
+    .setDescription('Replies with Polo!'),
+  async execute(interaction){
+    await interaction.reply('Polo!');
+  }
+};
